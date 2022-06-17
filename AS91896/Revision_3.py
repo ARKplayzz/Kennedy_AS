7525cd745f1546389710a5095fe682c682bcecaf
Andrew <74849088+ARKplayzz@users.noreply.github.com>
2022-06-17 14:58:49 +1200

    Update Revision_3.py

--- AS91896/Revision_3.py
+++ AS91896/Revision_3.py
@@ -1,5 +1,5 @@
 '''
-Password Safe - Revision one
+Password Safe - Revision three
 This programme is used to meet the breif of the 2.7 Password safe programme AS91896
 by Andrew Kennedy
 5/05/2022
@@ -8,8 +8,9 @@ by Andrew Kennedy
 # Import Modules - Modules can be used to implement external features like "random" or "json"
 import json
 
+
 # Defining Constants - Constants are variables that stay the same during the entirety of the programme 
-USER_LOGIN = [['admin', 1234],['ARKennedy', 3605]]
+USER_LOGIN = ['admin', 1234]
 
 PASS_MAX_LENGTH = 16
 PASS_MIN_LENGTH = 4
@@ -54,22 +55,18 @@ def pass_login(): # This Function runs a login proccedure for the user and retur
 
     user = get_string('NOTICE: Please input your username >>> ')
 
-    for i in range(len(USER_LOGIN)):
+    if user == USER_LOGIN[0]:
 
-        if user == USER_LOGIN[i][0]:
+        pin = get_pin()
 
-            pin = get_pin()
-
-            if pin == USER_LOGIN[i][1]:
-                return True
-
-            else:
-                print(f'ALERT: That pin is incorrect for the user "{user}"')
-                break
+        if pin == USER_LOGIN[1]:
+            return True
 
         else:
-            print(f'ALERT: "{user}" Is not currently a known username')
-            break
+            print(f'ALERT: That pin is incorrect for the user "{user}"')
+
+    else:
+        print(f'ALERT: "{user}" Is not currently a known username')
 
 def menu_opt_1(): # This Function Finds the users inputted WEBSITE and provides the corresponding PASSWORD
     web_lookup = get_string('NOTICE: Enter the website you want to lookup the password for >>> ')
@@ -78,6 +75,8 @@ def menu_opt_1(): # This Function Finds the users inputted WEBSITE and provides 
     with open ('passwords.json','r') as passwords_json: 
         website_list = json.load(passwords_json)
         passwords_json.close()
+
+    issue_check = True
 
     # for every website in the json file, the programme checks for a matching website
     for counter in range(len(website_list['websites'])):
@@ -91,52 +90,122 @@ def menu_opt_1(): # This Function Finds the users inputted WEBSITE and provides 
 
             print(f'NOTICE: The password for the website: {requested_website} is {requested_pasword}')
 
-    print('ALERT: Please input a Website That already has a password inputed with it!')
+            issue_check = False
+    
+    if issue_check != False:
+
+        print('ALERT: Please input a Website That already has a password inputed with it!')
 
 def menu_opt_2(): # This Function Creates a new WEBSITE along with a new PASSWORD using values provided by the User
     web_log = get_string('NOTICE: Please input the name of the website you would like to add a password for >>> ')
 
-    for website in range(len(WEBSITE_LIST)):
-        if web_log.lower() == WEBSITE_LIST[website][0].lower():
-            print(f'ALERT: The website: {WEBSITE_LIST[website][0]} is already logged whithin the programme')
+    # Appends the json file containing passwords to the variable: website_list
+    with open ('passwords.json','r') as passwords_json: 
+        website_list = json.load(passwords_json)
+        passwords_json.close()
+
+    for counter in range(len(website_list['websites'])):
+
+        requested_website = website_list['websites'][f'website_{counter}']['website']
+
+        if web_log.lower() == requested_website.lower():
+
+            print(f'ALERT: The website: {web_log} is already logged whithin the programme')
             print(f'ALERT: Try selecting option [3] to edit an existing websites password')
+        
+        else:
 
-    pass_log = get_pass('NOTICE: Please enter your new password, Eg. Reflection27 >>> ')
+            pass_log = get_pass('NOTICE: Please enter your new password, Eg. Reflection27 >>> ')
 
-    WEBSITE_LIST.append([web_log, pass_log])
+            #website_list['websites'][f'website_{counter + 1}']['website'] = web_log
+            website_list['websites'] = website_list[f'website_{counter + 1}']['website'] = web_log
+            website_list['websites'] = website_list[f'website_{counter + 1}']['password'] = pass_log
 
-    print(f'NOTICE: The password {pass_log} has been saved to the website {web_log}')
+            print(f'NOTICE: The password {pass_log} has been saved to the website {web_log}')
+
+            with open ('passwords.json', 'w') as file:
+                json.dump(website_list, file, indent=4)
+                file.close()
 
 def menu_opt_3(): # This Function Edits an existing PASSWORD of the users provided WEBSITE using the value given
 
     web_log = get_string('NOTICE: Please input the name of the website you would like to edit the password for >>> ')
 
-    for website in range(len(WEBSITE_LIST)):
+    # Appends the json file containing passwords to the variable: website_list
+    with open ('passwords.json','r') as passwords_json: 
+        website_list = json.load(passwords_json)
+        passwords_json.close()
+
+    fail_check = True
+
+    for counter in range(len(website_list['websites'])):
+
+        requested_website = website_list['websites'][f'website_{counter}']['website']
         
-        if web_log.lower() != WEBSITE_LIST[website][0].lower():
-            print(f'ALERT: The website {web_log} does not exist, try selecting option [2] to add a new website & password')
+        if web_log.lower() != requested_website.lower():
+            continue
 
-    pass_log = get_pass(f'NOTICE: Please enter your new password for {web_log}, Eg. Reflection27 >>> ')
-    WEBSITE_LIST[website][1] = pass_log
-    print(WEBSITE_LIST[website][1])
-    print(WEBSITE_LIST)
+        else:
 
-    print(f'NOTICE: The password {pass_log} has been saved to the website {web_log}')
+            pass_log = get_pass(f'NOTICE: Please enter your new password for {web_log}, Eg. Reflection27 >>> ')
+
+            website_list['websites'][f'website_{counter}']['password'] = pass_log
+
+            print(f'NOTICE: The password {pass_log} has been saved to the website {web_log}')
+
+            with open ('passwords.json', 'w') as file:
+                json.dump(website_list, file, indent=4)
+                file.close()
+            
+            fail_check = False
+
+    if fail_check != False:
+        print(f'ALERT: The website {web_log} does not exist, try selecting option [2] to add a new website & password')
 
 def menu_opt_4(): # This Function removes an existing WEBSIT along with its PASSWORD using the users inputted value
 
     web_log = get_string('NOTICE: Please input the name of the website you would like to remove from the programme >>> ')
 
-    for website in range(len(WEBSITE_LIST)):
+    with open ('passwords.json','r') as passwords_json: 
+        website_list = json.load(passwords_json)
+        passwords_json.close()
+
+    issue_check = True
+
+    for counter in range(len(website_list['websites'])):
+
+        requested_website = website_list['websites'][f'website_{counter}']['website']
         
-        if web_log.lower() != WEBSITE_LIST[website][0].lower():
-            print(f'ALERT: The website {web_log} does not exist, try selecting option [2] to add a new website & password')
+        if web_log.lower() != requested_website.lower():
+            continue           
 
-    
-    del(WEBSITE_LIST[website]) #include in documentation
-    print(WEBSITE_LIST)
+        else:
 
-    print(f'NOTICE: The website: {web_log} has been successfuly removed from the programme')
+            del(website_list['websites'][f'website_{counter}']) #include in documentation
+            print(f'NOTICE: The website: {web_log} has been successfuly removed from the programme')
+
+            with open ('passwords.json', 'w') as file:
+                json.dump(website_list, file, indent=4)
+                file.close()
+            
+            issue_check = False
+
+    if issue_check != False:
+        print(f'ALERT: The website {web_log} does not exist, try selecting option [2] to add a new website & password')
+
+def menu_opt_6(): 
+    print('NOTICE: Here are all the currently stored Websites and Passwords in the Password Safe Programme') 
+
+    with open ('passwords.json','r') as passwords_json:  
+        website_list = json.load(passwords_json) 
+        passwords_json.close() 
+          
+    for counter in range(len(website_list['websites'])): 
+         
+        requested_website = website_list['websites'][f'website_{counter}']['website'] 
+        requested_password = website_list['websites'][f'website_{counter}']['password'] 
+
+        print(f'NOTICE: The Website "{requested_website}" has the Password "{requested_password}"') 
 
 # Main Programme -  This is where the base programme will run
 
@@ -149,28 +218,36 @@ while roadblock != True: # Roadblock refers to the user needing to login before 
 
 print('NOTICE: Welcome to the programme, you passed the roadblock')
 
-print('''
-----------------MENU---------------
-1. Find the password for an existing website/app
-2. Add a new website/app and new password for it
-3. Change an existing password for an existing website/app
-4. Remove an existing website/app and its password
-5. Exit
-6.
-''')
-menu_input = get_integer('>>> ')
+quit_checker = False
+while quit_checker != True:
 
-if menu_input == 1:
-    menu_opt_1()
+    print('''
+    ----------------MENU---------------
+    1. Find the password for an existing website/app
+    2. Add a new website/app and new password for it
+    3. Change an existing password for an existing website/app
+    4. Remove an existing website/app and its password
+    5. Exit
+    6. Display all know Websites along with their passwords
+    ''')
+    menu_input = get_integer('>>> ')
 
-elif menu_input == 2:
-    menu_opt_2()
+    if menu_input == 1:
+        menu_opt_1()
 
-elif menu_input == 3:
-    menu_opt_3()
+    elif menu_input == 2:
+        menu_opt_2()
 
-elif menu_input == 4:
-    menu_opt_4()
+    elif menu_input == 3:
+        menu_opt_3()
 
-elif menu_input == 5:
-    print('NOTICE: Goodbye!')
+    elif menu_input == 4:
+        menu_opt_4()
+
+    elif menu_input == 5:
+        quit_checker = True
+
+    elif menu_input == 6:
+        menu_opt_6()
+
+print('NOTICE: Thanks for using the Password Safe programme!')
